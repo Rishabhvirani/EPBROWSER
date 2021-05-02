830e8a798811a11c5029450f57c670c8f9ab514e
rishabh virani <rishabhvirani007@gmail.com>
2021-05-02 23:05:02 +0530

    Header update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
                 </div>
             </div>
         </div>
-        @livewire('navigation-dropdown')
+        @livewire('navigation-dropdown')    
         <div class="sub-header-container">
             <header class="header navbar navbar-expand-sm">
                 <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></a>
@@ -54,9 +54,7 @@
         </div>
         <div id="content" class="main-content">
             <div class="layout-px-spacing">
-                
                     {{$slot}}
-                
             </div>
         </div>
     </div>
--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -1,9 +1,8 @@
 
 
 
-<div  x-data="{ open: false }" class="header-container fixed-top">
+<div x-data="{ open: false }" class="header-container fixed-top">
         <header class="header navbar navbar-expand-sm">
-
             <ul class="navbar-item theme-brand flex-row  text-center">
                 <li class="nav-item theme-logo">
                     <a href="{{url('/dashboard')}}">
@@ -18,7 +17,7 @@
             <ul class="navbar-item flex-row ml-md-auto">
                 <li class="nav-item dropdown user-profile-dropdown">
                     <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                        
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
                     </a>
                     <div class="dropdown-menu position-absolute" aria-labelledby="userProfileDropdown">
                         <div class="">
